1ff6b524c85d54189177202a28a2977f6c20b40c
VirajTank <virajtank98@gmail.com>
2022-10-02 13:07:00 +0530

    added QRGenerator

--- app/src/main/java/com/novuspax/androidutilities/ui/qr_creator/QRMainActivity.kt
+++ app/src/main/java/com/novuspax/androidutilities/ui/qr_creator/QRMainActivity.kt
@@ -1,16 +1,27 @@
 package com.novuspax.androidutilities.ui.qr_creator
 
 import android.content.Intent
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
 import com.novuspax.androidutilities.R
 import com.novuspax.androidutilities.databinding.ActivityQrMainBinding
+import com.novuspax.androidutilities.utils.QRGenerator.QRGContents
+import com.novuspax.androidutilities.utils.QRGenerator.QRGEncoder
+import com.novuspax.androidutilities.utils.sdkAboveR
 
 class QRMainActivity : AppCompatActivity(), View.OnClickListener {
     private val binding by lazy {
         ActivityQrMainBinding.inflate(layoutInflater)
     }
+    private var width = 0
+    private var height = 0
+    private var dimen = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -26,12 +37,34 @@ class QRMainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View?) {
-        when(view){
+        when (view) {
             binding.btnGenerateQR -> {
-                startActivity(Intent(this@QRMainActivity,GenerateQRActivity::class.java))
+                sdkAboveR {
+                    val windowMetrics = windowManager.currentWindowMetrics
+                    val insets = windowMetrics.windowInsets.getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+                    width = windowMetrics.bounds.width()
+                    height = windowMetrics.bounds.height()
+                    dimen = if (width < height) width else height
+
+                } ?: run {
+                    val manager = getSystemService(WINDOW_SERVICE) as WindowManager
+                    val display = manager.defaultDisplay
+                    val point = Point()
+                    display.getSize(point)
+                    width = point.x
+                    height = point.y
+                    dimen = if (width < height) width else height
+                }
+
+                QRGEncoder(
+                    binding.etTextQR.text.toString(),
+                    null,
+                    QRGContents.Type.TEXT,
+                    dimen
+                )
             }
             binding.btnScanQR -> {
-                startActivity(Intent(this@QRMainActivity,ScanQRActivity::class.java))
+                startActivity(Intent(this@QRMainActivity, ScanQRActivity::class.java))
             }
         }
     }
--- app/src/main/java/com/novuspax/androidutilities/utils/Extension.kt
+++ app/src/main/java/com/novuspax/androidutilities/utils/Extension.kt
@@ -4,4 +4,11 @@ import android.app.DownloadManager
 import android.app.Service
 import android.content.Context
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
+
+inline fun <T> sdkAboveR(aboveRVersion: () -> T?): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        aboveRVersion()
+    } else null
+}
